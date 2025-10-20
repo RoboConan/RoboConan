3418348548aad21ddad9ea88bc312697bfd77951
Alberto Scolari <alberto.scolari88@gmail.com>
2025-10-20 10:55:08 +0200

    tree-sitter-cuda: Add new package (#28510)
    * adding recipe for tree-sitter-cuda
    
    * turning fixed version into range constraint
    
    * fixing test: CUDA code string is now correct
    
    * simplifying test code
    
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>
    
    * fixing homepage
    
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>
    
    ---------
    
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>
    (cherry picked from commit babae78a50c979e6509f6de4878cc0fe152f5143)

---
+++ recipes/tree-sitter-cuda/all/conanfile.py
@@ -0,0 +1,63 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import *
+
+required_conan_version = ">=2.4"
+
+
+class TreeSitterCUDAConan(ConanFile):
+    name = "tree-sitter-cuda"
+    description = "CUDA C++ grammar for tree-sitter"
+    license = "MIT"
+    homepage = "https://github.com/tree-sitter-grammars/tree-sitter-cuda"
+    topics = ("tree-sitter", "parser", "cuda")
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    implements = ["auto_shared_fpic"]
+    languages = ["C"]
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def requirements(self):
+        tree_sitter_version = self.conan_data["tree-sitter-version"][self.version]
+        self.requires(f"tree-sitter/{tree_sitter_version}", transitive_headers=True, transitive_libs=True)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        copy(self, "LICENSE", self.source_folder, os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "share", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.set_property("pkg_config_name", "tree-sitter-cuda")
+        self.cpp_info.libs = ["tree-sitter-cuda"]
+        self.cpp_info.resdirs = ["share"]
