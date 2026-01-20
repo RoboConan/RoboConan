276fb6512869d2c54e8bed4673acdb374ae89858
Uilian Ries <uilianr@jfrog.com>
2026-01-20 13:23:22 +0200

    boost: simplify test package (#26736)
    * Drop Visual Studio from boost recipe
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove _settings_build
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Update coroutine validate
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove legacy generators support
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Replace cppstd_flag
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add package type and required cppstd always
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove debug level option - use conan conf
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Simplify libcxx
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Reduce modules
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * disable process component on Windows
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove deprecated option i18n_backend
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove not used methods
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Do not use ctest for testing
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Simplify nowide test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add options for stacktrace
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add option for backtrace from exceptioN
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove deprecated patches
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Update log level to info
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove not used patches for apple-clang
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Do not build backtrace_from_exception for mingw
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Validate mingw for stacktrace from exception
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Skip build in case is header-only
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Simplify test package for Boost
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Delete leftover line
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove custom boost namespace when testing
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Do not run .exp when testing
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Boost stacktrace from exception needs 1.86 for Windows
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Disable Boost cobalt by default
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Remove new option for boost stacktrace from_exception
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Except stacktrace_from_exception for intel only
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Make sure linking libraries when testing
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix nowide test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Do not enforce package-type for header-only
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add support c++-latest as c++ std version
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Revert Boost recipe changes
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Simplify boost process test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Fix Windows build for boost process
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Add shell headers
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    ---------
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    (cherry picked from commit 7b7fa86db7f75130f1d74f4cae50f181098d1858)

--- recipes/boost/all/test_package/conanfile.py
+++ recipes/boost/all/test_package/conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conan import ConanFile
 from conan.tools.build import can_run
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
@@ -30,12 +31,8 @@ class TestPackageConan(ConanFile):
         tc.cache_variables["WITH_RANDOM"] = opts.with_random
         tc.cache_variables["WITH_REGEX"] = opts.with_regex
         tc.cache_variables["WITH_STACKTRACE"] = opts.with_stacktrace
-        tc.cache_variables["WITH_STACKTRACE_ADDR2LINE"] = self.dependencies["boost"].conf_info.get("user.boost:stacktrace_addr2line_available", False)
-        tc.cache_variables["WITH_STACKTRACE_BACKTRACE"] = opts.get_safe("with_stacktrace_backtrace", False)
         tc.cache_variables["WITH_TEST"] = opts.with_test
         tc.cache_variables["WITH_URL"] = opts.get_safe("with_url", True)
-        if opts.namespace != "boost" and not opts.namespace_alias:
-            tc.cache_variables["BOOST_NAMESPACE"] = opts.namespace
         tc.generate()
 
     def build(self):
@@ -46,15 +43,10 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not can_run(self):
             return
-        with chdir(self, self.folders.build_folder):
-            # When boost and its dependencies are built as shared libraries,
-            # the test executables need to locate them. Typically the
-            # `conanrun` env should be enough, but this may cause problems on macOS
-            # where the CMake installation has dependencies on Apple-provided
-            # system libraries that are incompatible with Conan-provided ones.
-            # When `conanrun` is enabled, DYLD_LIBRARY_PATH will also apply
-            # to ctest itself. Given that CMake already embeds RPATHs by default,
-            # we can bypass this by using the `conanbuild` environment on
-            # non-Windows platforms, while still retaining the correct behaviour.
-            env = "conanrun" if self.settings.os == "Windows" else "conanbuild"
-            self.run(f"ctest --output-on-failure -C {self.settings.build_type}", env=env)
+
+        for file in os.listdir(self.cpp.build.bindirs[0]):
+            if file.startswith("test_boost_"):
+                if self.settings.os == "Windows" and not file.endswith(".exe"):
+                    continue
+                bin_path = os.path.join(self.cpp.build.bindirs[0], file)
+                self.run(bin_path, env="conanrun")
