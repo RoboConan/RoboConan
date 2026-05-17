9a99bde2c7d46d58ad887398c90815f8cee422d3
Martin Valgur <martin.valgur@gmail.com>
2026-05-17 13:41:25 +0300

    pcsc-lite: flex is required

--- recipes/pcsc-lite/all/conanfile.py
+++ recipes/pcsc-lite/all/conanfile.py
@@ -56,6 +56,7 @@ class PCSCLiteConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("meson/[^1.2.3]")
+        self.tool_requires("flex/[^2.6]")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/[^2.2]")
 
